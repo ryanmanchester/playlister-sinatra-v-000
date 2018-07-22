25ee5b8ed1bfefe2a23a68b889bed38fb1778c88
Ryan Manchester <ryanm17@gmail.com>
2018-07-22 23:19:59 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,7 +4,7 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def slug
-    self.name.gsub(' ', '-').gsub(/[^\w-]/, '').gsub(/(-){2,}/, '-').downcase
+    self.name.gsub(' ', '-').downcase
   end
 
   def self.find_by_slug(slug)

ecee8797176d04339e7930b8007abfdfc92af5e4
Ryan Manchester <ryanm17@gmail.com>
2018-07-23 00:59:37 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -34,6 +34,7 @@ use Rack::Flash
   patch '/songs/:slug' do
 
     @song = Song.find_by_slug(params["slug"])
+    binding.pry
     @song.name = params["Name"]
     @song.artist = params["Artist Name"]
     @song.genre_ids = params[:genres]

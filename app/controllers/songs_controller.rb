1bdd33d87c87c7c9e13e8863694dc6f99482ec77
Ryan Manchester <ryanm17@gmail.com>
2018-07-23 01:41:03 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -37,9 +37,9 @@ use Rack::Flash
     @song.name = params["Name"]
     #binding.pry
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
-    #binding.pry
-    @song.genre_ids = params[:genres]
     binding.pry
+    @song.genre_ids = params[:genres]
+    #binding.pry
     @song.save
     flash[:message] = "Successfully updated song."
     redirect "/songs/#{@song.slug}"

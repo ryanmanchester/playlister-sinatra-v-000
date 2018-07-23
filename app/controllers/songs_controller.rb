55093186b4e2b2cdf1777b44c9a6d19d7bf542ae
Ryan Manchester <ryanm17@gmail.com>
2018-07-23 00:53:19 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -34,8 +34,7 @@ use Rack::Flash
   patch '/songs/:slug' do
 
     @song = Song.find_by_slug(params["slug"])
-    @song.update(artist: params["Artist Name"], genre_ids: params["genres"])
-
+    @song.update(artist: params["Artist Name"])
 
 
     flash[:message] = "Successfully updated song."
